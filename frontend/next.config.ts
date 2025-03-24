c15932ac2be2a193fc35a8c65dd7bcfae3b3b279
kairo913 <148944926+kairo913@users.noreply.github.com>
2025-03-24 11:13:39 +0900

    Update to the latest Nextjs and tailwindcss

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
-    distDir: "dist",
     output: "export",
+    allowedDevOrigins: ["wails.localhost"]
 };
 
 export default nextConfig;
